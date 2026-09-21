e41cf6ac5978d5f991452b80e50cbd366888f92c
SabrinaHF <sabrinahoflor@gmail.com>
2026-09-20 21:46:43 -0300

    Altera valor do teste para provocar falha no CI

--- src/test/java/com/sabrina/devops/SeguroTest.java
+++ src/test/java/com/sabrina/devops/SeguroTest.java
@@ -17,7 +17,7 @@ public class SeguroTest {
     @Test
     public void valorAcimaDoLimite(){
 
-        String resultado = Seguro.verificarCobertura(1000000);
+        String resultado = Seguro.verificarCobertura(1000);
         assertEquals("Valor fora do limite de cobertura do FGC!", resultado);
     }
 
